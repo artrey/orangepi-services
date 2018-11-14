ad247a96e45baa812af4b970d985d1710a829deb
Alexander Ivanov <oz.sasha.ivanov@gmail.com>
2018-11-14 22:04:21 +0300

    Finished run-all script

--- run-all.sh
+++ run-all.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env bash
 
-# find root dir of script
+echo "Detecting root dir of script..."
 ROOT_DIR="$( cd "$( dirname "$0" )" >/dev/null && pwd )"
-cd "$ROOT_DIR"
+echo "Script folder: $ROOT_DIR"
 
-# creating media folder for all media files (such as movies, serials, music, etc.)
-if [ ! -d "media" ]; then
-  mkdir "media"
-fi
-
-bash fan-manager/run.sh
-bash samba/run.sh
-bash transmission/run.sh
-bash plex/run.sh
+echo "Running services..."
+bash "$ROOT_DIR/fan-manager/run.sh"
+bash "$ROOT_DIR/samba/run.sh"
+bash "$ROOT_DIR/transmission/run.sh"
+bash "$ROOT_DIR/plex/run.sh"
